b13cf32e8dd48e76e6767b254ca4e97b164c4366
TharothTHO <tharoth.tho@gmail.com>
2022-03-01 12:29:42 +0700

    modify _slug

--- content/articles/my-first-blog-post.md
+++ content/articles/my-first-blog-post.md
@@ -1,4 +1,14 @@
-# My first blog post
 
-Welcome to my first blog post using content module
+---
+title: Create a blog with VueJS and NuxtJS
+short: How do you create an SEO optimized static file website with NuxtJS and markdown files, together with an sitemap.xml and tag pages.
+image: /images/my-header-image.jpg
+tags:
+  - code
+  - nuxt
+  - markdown
+---
 
+## My blog title
+
+Your usual **markdown** content. 
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,26 +1,25 @@
 <template>
-  <article>
+  <div>
+    <div class="post-head">
+      <h1>{{article.title}}</h1>
+      <img :src="article.image" v-if="article.image">
+      <div>
+        <div class="date">{{article.createdAt}}</div>
+        <div class="tags">
+          <span v-for="tag in article.tags" :key="tag">
+            <nuxt-link :to="'/tag/'+tag">\#{{tag}}</nuxt-link>
+          </span>
+        </div>
+      </div>
+    </div>
     <nuxt-content :document="article" />
-  </article>
+  </div>
 </template>
-
-
 <script>
   export default {
-    async asyncData({ $content, params }) {
+    async asyncData ({ $content, params }) {
       const article = await $content('articles', params.slug).fetch()
-
       return { article }
     }
   }
 </script>
-
-<style>
-  .nuxt-content h2 {
-    font-weight: bold;
-    font-size: 28px;
-  }
-  .nuxt-content p {
-    margin-bottom: 20px;
-  }
-</style>
